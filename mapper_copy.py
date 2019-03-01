cf065caf0276a10ec918875434951bbc4767b2a8
Lo1gr <louis.gruand@free.fr>
2019-03-01 15:23:30 +0100

    Update mapper_copy.py

--- mapper_copy.py
+++ mapper_copy.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import sys
 
-#actual mapping algorithm that needs A_i, B_i and text file as input
+
 for line in sys.stdin:
     A_i = 600
     B_j = 600
@@ -11,8 +11,8 @@ for line in sys.stdin:
     if matrix == "A":
         for ind in range(1,B_j+1):
             key = i + "," + str(ind)
-            print '%s\t%s\t%s' % (key,j,v)
-            #we use tab to get rid of the problem of negative number
+            print '%s\t%s\t%s' % (key, j, v)
+
     else:
         for ind in range(1,A_i+1):
             key = str(ind) + "," + j
