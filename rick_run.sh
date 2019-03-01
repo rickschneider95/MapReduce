84ee5484ce93c085cc339a0aa3e64bea48e5d590
Lo1gr <louis.gruand@free.fr>
2019-03-01 15:12:05 +0100

    Update rick_run.sh

--- rick_run.sh
+++ rick_run.sh
@@ -1,6 +1,6 @@
 hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar \
--input /user/hadoop/rick/input/600x600.txt\
--output /user/hadoop/rick/output\
+-input /user/hadoop/rick/input/600x600.txt \
+-output /user/hadoop/rick/output \
 -file /home/hadoop/mapper.py \
 -mapper /home/hadoop/mapper.py \
 -file /home/hadoop/reducer.py \
